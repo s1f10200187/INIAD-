d304eb22ad23cda4730b87bd88805740b08fb670
Yuito Furuhata <y.furuhata1131@gmail.com>
2024-07-20 15:54:49 +0900

    Update class_sort_button.js

--- js/class_sort_button.js
+++ js/class_sort_button.js
@@ -17,17 +17,9 @@ $(document).on("click", "#download-slide-all", function(){
         }
         console.log(a_tags_href);
 
-        // a_tags_hrefのすべてのurlにアクセスし、helloをalertする
-        for(let i = 0; i < a_tags_length; i++){
-            $.ajax({
-                url: a_tags_href[i],
-                type: "GET",
-                success: function(data){
-                    alert("hello");
-                }
-            });
-        };
-
+        // https://moocs.iniad.org/courses/2023/BTHESISへ遷移
+        window.location.href = 'https://example.com';
+        
         // let registclasses = $(".well");
         // let textsh4 = $(registclasses).find(".media-heading");
         // let l_txt = [];
